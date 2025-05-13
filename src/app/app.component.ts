0d0ac962469a7ce8fb2890e9ae3b2e7487615477
nandini <nandani@contaque.com>
2025-05-13 15:06:03 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { NgIf, NgIfContext } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'templatedrivenvalidation25';
+userdetails:any;
+  addDetails(val:NgForm){
+console.log(val.name);
+this.userdetails=val;
+  }
+  // template driven wale case me humare validation .html wale file i.e template me jake lagte hai and reactive wale case me .ts me lagte hai
 }
